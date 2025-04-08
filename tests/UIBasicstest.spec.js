a8a79d6db7e5c3f56b7520430e1ea9961501abe7
Aliaa osama <aliaaosama97@gmail.com>
2025-04-08 15:12:26 +0200

    chapter 5 done open new page

--- tests/UIBasicstest.spec.js
+++ tests/UIBasicstest.spec.js
@@ -31,15 +31,53 @@ test('Browser Context Playeright test', async ({browser})=>
 });
 
 
-test.only('UI Controls', async ({page})=> 
+test('UI Controls', async ({page})=> 
   {
   
     await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
     const userName = page.locator('#username');
    const signIn = page.locator('#signInBtn');
    const dropdown = page.locator("select[class='form-control']");
+   const radioButton =  page.locator(".radiotextsty");
+   const tearmsCheck = page.locator("#terms");
+   const documentLink = page.locator(".blinkingText");
    await dropdown.selectOption("Consultant");
-   await page.locator('document.querySelector("label:nth-child(2)")')
-   await page.pause();
+   await page.locator(".radiotextsty").last().click();
+   await page.locator("#okayBtn").click();
 
+   console.log(await radioButton.last().isChecked());
+   await expect(radioButton.last()).toBeChecked();
+   await tearmsCheck.click();
+   await expect(tearmsCheck).toBeChecked();
+   await tearmsCheck.uncheck();
+   expect(await tearmsCheck.isChecked()).toBeFalsy();
+   await expect(documentLink).toHaveAttribute("class", "blinkingText");
+
+
+   ///assertion
+   //await page.pause();
+    //
+  });
+
+
+  test.only('Child windows hadl', async ({browser})=>
+  {
+    //to open a new page through a link or any clikable items & split to get the mail
+    const context = await browser.newContext();
+   const page = await context.newPage();
+   const userName = page.locator('#username');
+    await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+    const documentLink = page.locator(".blinkingText");
+    const [newPage] = await Promise.all(
+    [
+      context.waitForEvent('page'),
+      documentLink.click(),
+    ])
+     const text =await newPage.locator(".red").textContent();
+     const arrayText = text.split("@");
+     const domain = arrayText[1].split(" ")[0];
+    console.log(domain);
+    await page.locator("#username").fill(domain)
+    await page. pause();
+    console.log(await page.locator("#username").textContent());
   });
