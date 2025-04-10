21ff969aa3662945b6c4e4192747fc8125dadf47
Aliaa osama <aliaaosama97@gmail.com>
2025-04-10 15:15:34 +0200

    chapter 8 1st part

--- tests/UIBasicstest.spec.js
+++ tests/UIBasicstest.spec.js
@@ -82,7 +82,7 @@ test('UI Controls', async ({page})=>
     console.log(await page.locator("#username").textContent());
   });
 
-  test.only('Client App login', async ({page}) => {
+  test('Client App login', async ({page}) => {
     const email = "anshika@gmail.com";
     const productName = "ZARA COAT 3"
     const products = page.locator(".card-body");
@@ -145,3 +145,34 @@ test('UI Controls', async ({page})=>
 
    await page.pause();
   });
+
+  test('@Webst Client App login', async ({ page }) => {
+    //js file- Login js, DashboardPage
+    const email = "anshika@gmail.com";
+    const productName = 'ZARA COAT 3';
+    const products = page.locator(".card-body");
+    await page.goto("https://rahulshettyacademy.com/client");
+    await page.getByPlaceholder("email@example.com").fill(email);
+    await page.getByPlaceholder("enter your passsword").fill("Iamking@000");
+    await page.getByRole('button',{name:"Login"}).click();
+    await page.waitForLoadState('networkidle');
+    await page.locator(".card-body b").first().waitFor();
+    
+    await page.locator(".card-body").filter({hasText:"ZARA COAT 3"})
+    .getByRole("button",{name:"Add to Cart"}).click();
+  
+    await page.getByRole("listitem").getByRole('button',{name:"Cart"}).click();
+  
+    //await page.pause();
+    await page.locator("div li").first().waitFor();
+    await expect(page.getByText("ZARA COAT 3")).toBeVisible();
+  
+    await page.getByRole("button",{name :"Checkout"}).click();
+  
+    await page.getByPlaceholder("Select Country").pressSequentially("ind");
+  
+    await page.getByRole("button",{name :"India"}).nth(1).click();
+    await page.getByText("PLACE ORDER").click();
+  
+    await expect(page.getByText("Thankyou for the order.")).toBeVisible();
+ })
