715421306bdad2a19101d6abb2c420d458474256
Aliaa osama <aliaaosama97@gmail.com>
2025-05-08 11:23:29 +0200

    CHAPTER14 done

--- tests/ClientAppPO.spec.js
+++ tests/ClientAppPO.spec.js
@@ -1,8 +1,10 @@
 const {test, expect} = require('@playwright/test');
+const {customtest} = require('../utils/test-base');
 const {POManager} = require('../pageobjects/POManager');
+const dataset = JSON.parse(JSON.stringify(require("../utils/placeorderTestData.json")));
 
 
-test('Client App login', async ({page})=>
+test(`Client App login for ${dataset.productName}`, async ({page})=>
 {
   const poManager = new POManager(page);
    //js file- Login js, DashboardPage
@@ -12,13 +14,13 @@ test('Client App login', async ({page})=>
     const products = page.locator(".card-body");
     const loginPage = poManager.getLoginPage();
     await loginPage.goTo();
-    await loginPage.validLogin(username,password);
+    await loginPage.validLogin(dataset.username,dataset.password);
     const dashboardPage = poManager.getDashboardPage();
-    await dashboardPage.searchProductAddCart(productName);
+    await dashboardPage.searchProductAddCart(dataset.productName);
     await dashboardPage.navigateToCart();
 
    const cartPage = poManager.getCartPage();
-   await cartPage.VerifyProductIsDisplayed(productName);
+   await cartPage.VerifyProductIsDisplayed(dataset.productName);
    await cartPage.Checkout();
 
    const ordersReviewPage = poManager.getOrdersReviewPage();
@@ -36,7 +38,38 @@ test('Client App login', async ({page})=>
 });
 
 
-
+customtest.only('Client App login', async ({page,testDataForOrder})=>
+  {
+    const poManager = new POManager(page);
+     //js file- Login js, DashboardPage
+      const username = "anshika@gmail.com";
+      const password = "Iamking@000"
+      const productName = 'Zara Coat 4';
+      const products = page.locator(".card-body");
+      const loginPage = poManager.getLoginPage();
+      await loginPage.goTo();
+      await loginPage.validLogin(testDataForOrder.username,testDataForOrder.password);
+      const dashboardPage = poManager.getDashboardPage();
+      await dashboardPage.searchProductAddCart(testDataForOrder.productName);
+      await dashboardPage.navigateToCart();
+  
+     const cartPage = poManager.getCartPage();
+     await cartPage.VerifyProductIsDisplayed(testDataForOrder.productName);
+     await cartPage.Checkout();
+  
+     const ordersReviewPage = poManager.getOrdersReviewPage();
+     await ordersReviewPage.searchCountryAndSelect("ind","India");
+     const orderId = await ordersReviewPage.SubmitAndGetOrderId();
+    console.log(orderId);
+    await dashboardPage.navigateToOrders();
+    const ordersHistoryPage = poManager.getOrdersHistoryPage();
+    await ordersHistoryPage.searchOrderAndSelect(orderId);
+    expect(orderId.includes(await ordersHistoryPage.getOrderId())).toBeTruthy();
+  
+     //Zara Coat 4
+  
+  
+  });
 
 
 
